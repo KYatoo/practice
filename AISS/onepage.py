9a6a8dad8b16f2e15c010b7376052cbb2ca71637
KYatoo <KYatoo@users.noreply.github.com>
2020-01-14 21:40:33 +0800

    Update onepage.py

--- AISS/onepage.py
+++ AISS/onepage.py
@@ -15,8 +15,21 @@ def GetPicUrl(url):
 def GetPageList(url):
     res = UrlInit(url)
     html = etree.HTML(res)
-    pagelist = html.xpath('//div[@id="pages"]/a/@href')
-    pagelist = list(set(pagelist))
+    page1 = html.xpath('//div[@id="pages"]/a[1]/@href')[0].encode().decode('utf-8')
+    # print(type(page1))
+    pagelistget = html.xpath('//div[@id="pages"]/a/@href')
+    pagenum = []
+    for i in pagelistget:
+        i = i.encode().decode('utf-8')
+        i = i.replace(page1, '').replace('.html', '')
+        if i != '':
+            pagenum.append(int(i))
+    maxnum = max(pagenum)
+    # print(maxnum)
+    pagelist = [page1]
+    if maxnum > 1 :
+        for num in range(2,maxnum+1):
+            pagelist.append(page1+ str(num) +".html")
     # print(pagelist)
     return pagelist
 
@@ -30,10 +43,9 @@ def DloadAtlas(url,name = "图集"):
         picurllist = GetPicUrl(pagelist[page])
         for pic in picurllist:
             DloadPic(pic,path,str(i))
-            i = i + 1
-            print("第%d张图下载完成" %i)
+            i = i + 1            print("第%d张图下载完成" %i)
 
 if __name__ == "__main__":
     # GetPicUrl('https://www.meituri.com/a/13670/')
     DloadAtlas('https://www.meituri.com/a/13658/')
-
+    # print (GetPageList('https://www.meituri.com/a/13658/'))
