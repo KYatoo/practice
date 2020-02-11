45bf315880d88ce316f998d135ec78810013ebff
KYatoo <34603040+KYatoo@users.noreply.github.com>
2020-02-11 19:21:36 +0800

    Update boom.py

--- boomweb/boom.py
+++ boomweb/boom.py
@@ -1,18 +1,11 @@
-import json
-import requests
-import webbrowser
-import random
-
+import requests,random
 while True:
     poster = random.sample('abcdefghijklmnopqrstuvwxyz!@#$%^&*()',5)
-    poster = "".join(posterx)
+    poster = "".join(poster)
     content = random.sample('abcdefghijklmnopqrstuvwxyz!@#$%^&*()',10)
     content = "".join(content)
-    print(content)
     url = 'https://paste.ubuntu.com/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
     post_data = {"poster": poster,"syntax": "text","expiration": "day","content": content}
     resp = requests.post(url,data=post_data,headers=headers)
-    print(type(resp))
-    print(resp.url)
-    # webbrowser.open(resp.url)
+    
