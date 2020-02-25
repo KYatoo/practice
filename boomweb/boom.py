14ebe585fd099f2d509d790952ea1b5e617ce541
KYatoo <34603040+KYatoo@users.noreply.github.com>
2020-02-25 10:47:05 +0800

    Update boom.py

--- boomweb/boom.py
+++ boomweb/boom.py
@@ -8,4 +8,4 @@ while True:
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
     post_data = {"poster": poster,"syntax": "text","expiration": "day","content": content}
     resp = requests.post(url,data=post_data,headers=headers)
-    
+    # print(resp.url)
