fc7b627f80d777423483d90f060bf15f9cb9dff7
KYatoo <34603040+KYatoo@users.noreply.github.com>
2020-02-25 10:47:51 +0800

    test

---
+++ testtest/teststr.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 23:36:21 2020
+
+@author: HR
+"""
+
+import string  # string模块，包括ascii的字符序列
+import random  # random模块，随机
+
+
+def random_string(size=10, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+# string.ascii_uppercase=英文字母
+# string.digits=阿拉伯数字
+# ''.join(['a','b'])将随机字符拼凑成字符串
+
+# if __name__ == "__main__":
+print(random_string())
